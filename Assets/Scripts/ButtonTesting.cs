c59bc911169e5eb518e760c5cdeb7c6355359584
dmitterberger <43745493+dmitterberger@users.noreply.github.com>
2023-12-05 14:40:36 +0100

    clean up folderstructure

---
+++ Assets/Scripts/ButtonTesting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTesting : MonoBehaviour
+{
+
+    public void ButtonPrint(string message)
+    {
+        Debug.Log(message);
+    }
+}
